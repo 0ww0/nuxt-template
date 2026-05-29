bdabe0a2477c170d9f21c1feeb959c20361a42bf
was-war <was.war+github@outlook.com>
2026-05-29 14:01:36 +0800

    feat: add Docker containerization with Caddy proxy and integrate NuxtHub with database and storage support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxthub/core'],
+
+  hub: {
+    db: 'postgresql',
+    blob: {
+      driver: 'fs',
+      dir: '.data/blob'
+    },
+    kv: true,
+    cache: true,
+  },
+
+  nitro: {
+    storage: {
+      kv: {
+        driver: 'fs',
+        base: '.data/kv'
+      },
+      cache: {
+        driver: 'fs',
+        base: '.data/cache'
+      }
+    }
+  }
 })
